0c81c717a4d208215868ef943f8c9248ac25cc88
sanuthb <sanuthbibin@gmail.com>
2026-06-27 08:59:15 +0530

    configured next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+
+  outputFileTracingIncludes: {
+    "/api/generate-pdf": ["node_modules/@sparticuz/chromium/bin/**/*"],
+  },
 };
 
 export default nextConfig;
